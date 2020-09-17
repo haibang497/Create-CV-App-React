fe9978e077117e35154b209d6f4cce7c78fea7d3
haibang497 <haibang497@gmail.com>
2020-09-17 14:33:32 +0700

    Updates

---
+++ precache-manifest.5fd5f559be4a6c68aab051434fe595ac.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c08633d614bbc0e9046afd44225a58d",
+    "url": "/Create-CV-App-React/index.html"
+  },
+  {
+    "revision": "c862f15d85d0856908d3",
+    "url": "/Create-CV-App-React/static/css/2.136c6445.chunk.css"
+  },
+  {
+    "revision": "c6db06e37e462f087409",
+    "url": "/Create-CV-App-React/static/css/main.8ab77056.chunk.css"
+  },
+  {
+    "revision": "c862f15d85d0856908d3",
+    "url": "/Create-CV-App-React/static/js/2.42f125e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Create-CV-App-React/static/js/2.42f125e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6db06e37e462f087409",
+    "url": "/Create-CV-App-React/static/js/main.0ecd3c65.chunk.js"
+  },
+  {
+    "revision": "c2a3aaa092e051bd2837",
+    "url": "/Create-CV-App-React/static/js/runtime-main.869b5813.js"
+  },
+  {
+    "revision": "30e63557928bc084431afcd6ca580946",
+    "url": "/Create-CV-App-React/static/media/arrow-circle-left.30e63557.svg"
+  },
+  {
+    "revision": "905223aa073bca1390cdcf6fd2bf66cd",
+    "url": "/Create-CV-App-React/static/media/arrow-circle-right.905223aa.svg"
+  },
+  {
+    "revision": "11e7e3095c3127a1628b343ca4f141f4",
+    "url": "/Create-CV-App-React/static/media/email.11e7e309.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Create-CV-App-React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Create-CV-App-React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Create-CV-App-React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Create-CV-App-React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Create-CV-App-React/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "71c5c1e0c380ccd69286c947c585716d",
+    "url": "/Create-CV-App-React/static/media/fox.71c5c1e0.jpg"
+  },
+  {
+    "revision": "b8233af209a7fa9efbb844dd948f0fbc",
+    "url": "/Create-CV-App-React/static/media/square1.b8233af2.svg"
+  }
+]);
